f8c7b879f35f22924feaf980c26d724c3187aec0
BrunoDunay <cesardunah@gmail.com>
2025-05-13 20:02:51 -0600

    ejercicio 2

---
+++ semana2/ejercicio2.js
@@ -0,0 +1,39 @@
+const {ask} = require('../helpers/input');
+
+async function main() {
+        let opcion = "";
+        let frutas = [];
+
+        while (opcion !== '3'){
+            opcion = await ask("\n1. Agregar fruta\n2. Quitar fruta\n3. Salir ");
+
+            if (opcion === '1') {
+                const frutaNueva = await ask("¿Qué fruta quieres agregar? ");
+                frutas.push(frutaNueva);
+                console.log(`Fruta ${frutaNueva} agregada.`);
+                console.log(`Frutas: ${frutas}`);       
+            } else if (opcion === '2'){
+                if (frutas.length === 0) {
+                    console.log("No hay frutas para quitar.");
+                    continue
+                } else {
+                    let frutaQuitar = await ask("¿Qué fruta quieres quitar? ");
+                    let index = frutas.indexOf(frutaQuitar);
+
+                    if (index !== -1) {
+                        frutas.splice(index, 1);
+                        console.log(`Fruta ${frutaQuitar} Eliminada.`);
+                    }
+                    
+                    console.log(`Frutas: ${frutas}`);
+                } 
+            } else if (opcion === '3') {
+                console.log("Saliendo...");
+                console.log(`Tus frutas fueron: ${frutas}`);
+            } else {
+                console.log("Opción no válida. Intenta de nuevo.");
+            }   
+        } 
+}
+
+main();
---
+++ semana2/ejercicio3.js
@@ -0,0 +1,2 @@
+const {ask} = require('../helpers/input');
+
