693cfe1fd48b5a8aacc0d759f401d325be72fada
BrunoDunay <cesardunah@gmail.com>
2025-05-14 19:28:36 -0600

    ejercicio3

--- semana2/ejercicio3.js
+++ semana2/ejercicio3.js
@@ -1,2 +1,15 @@
 const {ask} = require('../helpers/input');
 
+async function main() {
+    const nombres = ["Ana", "Roberto", "Luis", "Mariana", "Sol"];
+
+    for (let i = 0; i < nombres.length; i++) {
+        if (nombres[i].length > 4) {
+            console.log(`${nombres[i]} tiene más de 4 letras.`);
+        } else {
+            console.log(`${nombres[i]} tiene solo ${nombres[i].length} letras.`);
+        }
+    }
+}
+
+main();
--- semana2/tarea2_frutas.js
+++ semana2/tarea2_frutas.js
@@ -4,8 +4,8 @@ async function main() {
         let opcion = "";
         let frutas = [];
 
-        while (opcion !== '3'){
-            opcion = await ask("\n1. Agregar fruta\n2. Quitar fruta\n3. Salir\n ");
+        while (opcion !== '4'){
+            opcion = await ask("\n1. Agregar fruta\n2. Quitar fruta\n3. Ver frutas\n4. Salir\n ");
 
             if (opcion === '1') {
                 const frutaNueva = await ask("¿Qué fruta quieres agregar? ");
@@ -28,6 +28,10 @@ async function main() {
                     console.log(`Frutas: ${frutas}`);
                 } 
             } else if (opcion === '3') {
+                for (let i = 0; i < frutas.length; i++) {
+                    console.log(`Fruta ${i + 1}: ${frutas[i]}`);
+                }
+            } else if (opcion === '4') {
                 console.log("Saliendo...");
                 console.log(`Tus frutas fueron: ${frutas}`);
             } else {
